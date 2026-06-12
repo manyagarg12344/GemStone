6b5ab93d1de1860c56123b17de790574248791d6
manyagarg12344 <156235028+manyagarg12344@users.noreply.github.com>
2026-06-13 00:39:04 +0530

    fix: increase API timeout

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -6,7 +6,7 @@ import axios from "axios";
  */
 const api = axios.create({
   baseURL: "https://hamara-pandit.onrender.com/api",
-  timeout: 15000,
+  timeout: 60000,
   headers: { "Content-Type": "application/json" },
 });
 
